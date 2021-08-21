7762ddc139eb7bfc530f115f43e87de6b6a5cc16
Assault-OPS <46131592+Assault-OPS@users.noreply.github.com>
2021-08-21 21:41:53 +0530

    Optimized Code

--- krishna.cpp
+++ krishna.cpp
@@ -1,95 +1,65 @@
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <string>
+//#define log(x) std::cout << x << std::endl;
+//optimizing my bar graph code
 
 
+class basic_utils {
+public:
+	static const int max(std::vector<int>& vecarray) {
+		int max_val = 0;
+		for (int i = 0; i < vecarray.size(); i++) {
+			if (max_val < vecarray[i]) 
+				max_val = vecarray[i];
+		}
+		return max_val;
+	}
+	static void process_data(std::vector<int>& processed,std::vector<int>& data) {
+		for (int i = 0; i < data.size(); i++) {
+			processed.emplace_back((data[i] * 10) / max(data));
+		}
+	}
+};
 
-int max(vector<int> test)
-{
-    int max_val = 0;
-    for(int i=0;i<test.size();i++)
-    {
-        if(max_val < test[i])
-            max_val = test[i];
-    }
-    return max_val;
-} 
 
-
-int main()
-{
-    vector<int> data;
-    vector<int> backup;
-    int limit;
-    int values;
-    int val;
-    //Input Data -------------------------
-    cout << "Enter limit: ";
-    cin >> limit;
-    for(int i=1;i<=limit;i++)
-    {
-        string x;
-        switch(i)
-        {
-            case 1: x = "st"; break;
-            case 2: x = "nd"; break;
-            case 3: x = "rd"; break;
-            default: x = "th"; break;
-        }
-        cout << "Enter " << i << x << " value: ";
-        cin >> values;
-        data.push_back(values);
-    }
-    backup = data;
-    //Data Converter------------------------
-    int bruh = max(data);
-    for(int i=0;i<data.size();i++)
-    {
-        val = (data[i]*10)/bruh; // <-- formula to convert data to graph form
-        data[i] = val;
-    }
-    // Bar Graph Maker (MAIN) --------------
-    int dynamic_bruh = to_string(bruh).length();
-    for(int i=1;i<11;i++)
-    {
-        cout << "|";  
-        if(dynamic_bruh == 1)
-            dynamic_bruh += 1;
-        for(int j=0;j<data.size();j++)
-        {
-            if(i>10-data[j])
-            {
-                for(int i=0;i<dynamic_bruh;i++)
-                    cout << "%";
-            }
-            else
-            {
-                for(int i=0;i<dynamic_bruh;i++)
-                    cout << " ";
-            }
-            cout << " ";
-        }
-        cout << endl;
-    }
-    //X-Axis--------------------------------
-    for(int i=0;i<data.size();i++)
-    {
-        for(int i=0;i<dynamic_bruh+1;i++)
-            cout << "-";
-    }
-    cout << endl << " ";
-    for(int i=0;i<backup.size();i++)
-    {
-        int num_len = to_string(backup[i]).length();
-        cout << backup[i];
-        for(int i=0;i<dynamic_bruh-num_len;i++)
-            cout << " ";
-        cout << " ";
-    }
-
-    cout << endl;
-
-    //--------------------------------------
+int main() {
+	int limit;
+	std::vector<int> data;
+	int max_len;
+	std::cout << "Enter Limit: " << std::endl;
+	std::cin >> limit;
+	data.reserve(50);
+	for (int i = 1; i <= limit; i++) {
+		const char* suffix;
+		unsigned int val;
+		switch (i) {
+		case 1: suffix = "st"; break;
+		case 2: suffix = "nd"; break;
+		case 3: suffix = "rd"; break;
+		default: suffix = "th"; break;
+		}
+		std::cout << "Enter " << i << suffix << " value: ";
+		std::cin >> val;
+		data.emplace_back(val);
+	}
+	data.shrink_to_fit();
+	max_len = basic_utils::max(data);
+	std::vector<int> processed_data;
+	processed_data.reserve(data.size());
+	basic_utils::process_data(processed_data, data);
+	int dynamic_width = std::to_string(max_len).length();
+	for (int i = 1; i < 11; i++)
+	{
+		std::cout << "|";
+		if (dynamic_width == 1)
+			dynamic_width += 1;
+		for (int j = 0; j < processed_data.size(); j++)
+		{
+			for (int k = 0; k < dynamic_width; k++) 
+				std::cout << (i > 10 - processed_data[j] ? "%" : " ");
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
 }
-
-
